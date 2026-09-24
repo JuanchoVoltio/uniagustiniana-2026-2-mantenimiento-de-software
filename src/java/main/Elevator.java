b4ff3b61d6e687d495ed5c46493212fc608b7b27
SamuelDBuendiaB <samuel.buendiab@uniagustiniana.edu.co>
2026-09-24 12:13:04 -0500

    Cambios en el Elevador

--- src/java/main/Elevator.java
+++ src/java/main/Elevator.java
@@ -3,17 +3,27 @@ class Elevator {
     public int MAX_LEVEL;
     public int MIN_LEVEL;
 
-    public Elevator(initialLevel) {
+    public Elevator(int initialLevel, int minLevel, int maxLevel) {
+        this.MIN_LEVEL = minLevel;
+        this.MAX_LEVEL = maxLevel;
         this.currentLevel = initialLevel;
     }
 
     public void goUp() {
-        System.out.println("Subiendo....")
-        currentLevel++;
+        if (currentLevel < MAX_LEVEL) {
+            System.out.println("Subiendo....");
+            currentLevel++;
+        } else {
+            System.out.println("Ya estás en el último piso.");
+        }
     }
 
     public void goDown() {
-        System.out.println("Bajando....")
-        currentLevel--;
+        if (currentLevel > MIN_LEVEL) {
+            System.out.println("Bajando....");
+            currentLevel--;
+        } else {
+            System.out.println("Ya estás en el primer piso.");
+        }
     }
 }
